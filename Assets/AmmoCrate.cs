90731b5bfaae60190ff2f7499c870df14678b3bd
AlSG00 <82202986+AlSG00@users.noreply.github.com>
2022-12-07 00:36:34 +0700

    v0.8.16
    Still working on inventory
    
    now player can interact with test items by using RaycastAll()
    
    TODO:
    - Learn to pick up and add items to inventory
    - Learn to drop itemsfrom the inventory
    - Learn to use items from the inventory

--- Assets/AmmoCrate.cs
+++ Assets/AmmoCrate.cs
@@ -4,64 +4,17 @@ using UnityEngine;
 
 public class AmmoCrate : MonoBehaviour
 {
-    //[SerializeField]
-    //private GameObject _object;
     [SerializeField] private int _ammo;
     private bool _isMouseOver;
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    //if (collision.gameObject.tag == "Player" && Input.GetKey(KeyCode.E) && isMouseOver)
-    //    //{
-    //    //    //collision.gameObject.GetComponent<Gun>().AddAmmo(ammo);
-    //    //    collision.gameObject.GetComponentInChildren<Gun>().AddAmmo(ammo);
-    //    //    Destroy(gameObject);
-    //    //}
 
-
-    //    if (collision.gameObject.tag == "Player"  )
-    //    {
-    //        Debug.Log("tag correct");
-    //        if (isMouseOver)
-    //        {
-
-    //            Debug.Log("key correct");
-    //            if (Input.GetKey(KeyCode.E))
-    //            {
-    //                Debug.Log("mouse aimed");
-    //                collision.gameObject.GetComponentInChildren<Gun>().AddAmmo(ammo);
-    //                Destroy(gameObject);
-    //            }
-    //        }
-    //    }
-    //}
     private void OnTriggerStay(Collider other)
     {
-        //RaycastHit hit;
-        //if (Physics.Raycast(other.transform.position, other.transform.forward, out hit))
-        //{
-        //    if (hit.transform.name == "AmmoCrate")
-        //    {
-
-                if (other.gameObject.tag == "Player" && Input.GetKey(KeyCode.E) && _isMouseOver)
-                {
-            //collision.gameObject.GetComponent<Gun>().AddAmmo(ammo);
-            //other.gameObject.GetComponentInChildren<SCRIPT_Weapon>().AddAmmo(_ammo);
+        if (other.gameObject.tag == "Player" && Input.GetKey(KeyCode.E) && _isMouseOver)
+        {
             other.gameObject.GetComponent<SCRIPT_PlayerAmmunition>().rifleAmmo.AddAmmo(_ammo);
-                    Destroy(gameObject);
-                }
-        //    }
-        //}
+            Destroy(gameObject);
+        }
     }
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.tag == "Player" && Input.GetKey(KeyCode.E) && isMouseOver)
-    //    {
-    //        //collision.gameObject.GetComponent<Gun>().AddAmmo(ammo);
-    //        other.gameObject.GetComponentInChildren<Gun>().AddAmmo(ammo);
-    //        Destroy(gameObject);
-    //    }
-
-    //}
 
     private void OnMouseEnter()
     {
---
+++ Assets/SCRIPT_ObjectInteraction.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class SCRIPT_ObjectInteraction : MonoBehaviour
+{
+    
+    RaycastHit[] hits;
+    Ray ray;
+    public LayerMask cursorLayer;
+    public LayerMask interactionLayer;
+    public float interactionDistance = 1f;
+    float currentDistance;
+    public GameObject player;
+    public SCRIPT_PickableObject pickableObject;
+    public SCRIPT_PickableObject previousPickableObject;
+    private void Awake()
+    {
+        player = GameObject.Find("_Player");
+    }
+
+    void Update()
+    {
+        MouseCursorRaycast();
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            ItemInteract();
+        }
+    }
+
+    private void MouseCursorRaycast()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        hits = Physics.RaycastAll(ray);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i].transform.CompareTag("Item"))
+            {
+                if (pickableObject != null)
+                {
+                    pickableObject.canPick = false;
+                }
+
+                pickableObject = hits[i].transform.GetComponent<SCRIPT_PickableObject>();
+                pickableObject.canPick = true;
+                break;
+            }
+            else if (hits.All(s => !s.transform.CompareTag("Item")))
+            {
+                pickableObject.canPick = false;
+                pickableObject = null;
+            }
+        }
+    }
+
+    private void ItemInteract()
+    {
+        if (pickableObject == null || pickableObject.canPick == false)
+        {
+            Debug.Log("Can't pick");
+            return;
+        }
+
+        pickableObject.Pick();
+    }
+}
