5ed32a4b1e245cc26d5283cc4fdd9710bc437044
AlSG00 <alex.gub2013@gmail.com>
2023-02-24 11:05:31 +0700

    v0.10.30 update 2
    Working on access implant...

--- Assets/Scripts/Implants/EyeAccessImplant.cs
+++ Assets/Scripts/Implants/EyeAccessImplant.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class EyeAccessImplant : MonoBehaviour, IImplant
 {
+    public List<AccessType.Type> AccessCollection = new List<AccessType.Type>();
+
+    public bool isActive = false;
+
     public void Activate()
     {
-
+        isActive = true;
     }
 
     public void Deactivate()
+    {
+        isActive = false;
+    }
+
+    public void AddAccessType(AccessType.Type accessType)
+    {
+        AccessCollection.Add(accessType);
+    }
+
+    public void RemoveAccessType()
     {
 
     }
