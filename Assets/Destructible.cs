3da24b783ad7e35c995a3657dd13eca112b0d0e5
AlSG00 <alex.gub2013@gmail.com>
2023-03-31 22:48:55 +0700

    v0.11.9
    Added explosion physic effect to destructable object

--- Assets/Destructible.cs
+++ Assets/Destructible.cs
@@ -6,13 +6,33 @@ public class Destructible : MonoBehaviour
 {
     public GameObject destroyedVersion;
 
+    public float explosionForce = 0.1f;
+    public float explosionRadius = 0.1f;
+    public float explosionUpward = 0.1f;
+
     public void DestroyObject()
     {
         Debug.Log("Instantiating...");
-        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        GameObject temp = Instantiate(destroyedVersion, transform.position, transform.rotation);
+
+        foreach (Transform child in temp.transform)
+        {
+            Rigidbody rb = child.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, explosionUpward);
+
+                Vector3 randomDirection = Random.insideUnitSphere;
+                randomDirection.y = Mathf.Abs(randomDirection.y);
+                rb.AddForce(randomDirection * Random.Range(0.5f, 1.5f), ForceMode.Impulse);
+            }
+        }
 
         Debug.Log("Destroying...");
         Destroy(gameObject);
+
+
     }
 
     private void OnMouseUp()
