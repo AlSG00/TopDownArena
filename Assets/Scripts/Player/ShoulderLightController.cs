4e1b40339e00c0107c71969862cc5e422d68301d
AlSG00 <alex.gub2013@gmail.com>
2023-06-08 22:25:52 +0700

    v0.12.11 backup 2

--- Assets/Scripts/Player/ShoulderLightController.cs
+++ Assets/Scripts/Player/ShoulderLightController.cs
@@ -8,39 +8,18 @@ public class ShoulderLightController : MonoBehaviour
 
     public Color _notificationColor;
 
-    private float _maxRgbValue = 255;
+    //private float _maxRgbValue = 255;
     private float _colorStep = 0f;
 
     public Color CurrentTestColor;
 
     public void Initialize(float maxParameterValue)
     {
-        //_colorStep = 255 / maxParameterValue;
         _colorStep = 1 / maxParameterValue;
     }
 
     public void SetHealthColor(float currentValue)
     {
-        //float resizedValue = currentValue * _colorStep;
-
-        //if (resizedValue < 0)
-        //{
-        //    resizedValue = 0;
-        //}
-        //else if (resizedValue > 255)
-        //{
-        //    resizedValue = 255;
-        //}
-
-        //_light.color = new Color32(
-        //    255 - resizedValue,
-        //    255 - (255 - resizedValue),
-        //    0,
-        //    255
-        //    );
-
-        //CurrentTestColor = _light.color;
-
         float resizedValue = currentValue * _colorStep;
 
         if (resizedValue < 0)
