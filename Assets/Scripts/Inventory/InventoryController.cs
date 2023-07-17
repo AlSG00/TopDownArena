ee2c402e9ec9097a375aaa8e7fa273c75f3b2ad3
AlSG00 <alex.gub2013@gmail.com>
2023-07-18 00:43:01 +0700

    v0.12.16
    Minor Entry_Garage improvements

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Linq;
 public class InventoryController : MonoBehaviour
 {
     private enum AudioPlayMode
